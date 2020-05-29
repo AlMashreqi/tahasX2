c9da29b6411ebacb659f601f571ae692b1204d6b
Sauood <37202095+sauoodwani@users.noreply.github.com>
2020-05-29 12:01:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ bot.ed_message = str()
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
-    await bot.change_presence(status = discord.Status.online, activity = discord.Game('8 Commands!'))
+    await bot.change_presence(status = discord.Status.online, activity = discord.Game('11 Commands!'))
 
 # @bot.event
 # async def on_command_error(ctx, error):
