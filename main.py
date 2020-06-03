94d7d29ae00f0f49aa941b3f689c2cc9cfe1c651
Sauood <37202095+sauoodwani@users.noreply.github.com>
2020-06-03 18:35:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -161,7 +161,7 @@ async def avatar_error(ctx, error):
 
 @bot.command(name = 'userinfo', help = f'Gives the info of the user')
 async def userinfo(ctx, member: discord.Member):
-    if member is None:
+    if member == None:
         member = ctx.message.author
     roles = [role for role in member.roles]
 
