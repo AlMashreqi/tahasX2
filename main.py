f019704359e0b51f59281237b6ab602095f00d22
Sauood <37202095+sauoodwani@users.noreply.github.com>
2020-06-03 18:17:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -160,7 +160,7 @@ async def avatar_error(ctx, error):
         await ctx.send('Please Specify a user!')
 
 @bot.command(name = 'userinfo', help = f'Gives the info of the user')
-async def userinfo(ctx, member = discord.Member):
+async def userinfo(ctx, member: discord.Member):
     roles = [role for role in member.roles]
 
     embed = discord.Embed(title = 'User Info', description = f'**{member.name}\'s Info**', color = bot.color_code, timestamp = ctx.message.created_at)
